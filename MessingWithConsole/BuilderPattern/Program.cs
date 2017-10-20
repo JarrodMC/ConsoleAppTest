bc981d8b268a76e7fa7995aa6e653286b80eb52f
Jarrod <JarrodMC@outlook.com>
2017-10-20 10:04:38 +0200

    Builder Pattern Code

--- MessingWithConsole/BuilderPattern/Program.cs
+++ MessingWithConsole/BuilderPattern/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BuilderPattern
 {
@@ -10,7 +7,108 @@ namespace BuilderPattern
     {
         static void Main(string[] args)
         {
-            //Builder Pattern Goes Here....
+            // Create director and builders
+            Director director = new Director();
+
+            Builder b1 = new ConcreteBuilder1();
+            Builder b2 = new ConcreteBuilder2();
+
+            // Construct two products
+            director.Construct(b1);
+            Product p1 = b1.GetResult();
+            p1.Show();
+
+            director.Construct(b2);
+            Product p2 = b2.GetResult();
+            p2.Show();
+
+            // Wait for user
+            Console.ReadKey();
         }
+
+        class Director
+        {
+            // Builder uses a complex series of steps
+            public void Construct(Builder builder)
+            {
+                builder.BuildPartA();
+                builder.BuildPartB();
+            }
+
+        }
+
+        abstract class Builder
+        {
+            public abstract void BuildPartA();
+            public abstract void BuildPartB();
+            public abstract Product GetResult();
+        }
+
+        /// <summary>
+        /// The 'ConcreteBuilder1' class
+        /// </summary>
+        class ConcreteBuilder1 : Builder
+        {
+            private Product _product = new Product();
+
+            public override void BuildPartA()
+            {
+                _product.Add("PartA");
+            }
+
+            public override void BuildPartB()
+            {
+                _product.Add("PartB");
+            }
+
+            public override Product GetResult()
+            {
+                return _product;
+            }
+        }
+
+        /// <summary>
+        /// The 'ConcreteBuilder2' class
+        /// </summary>
+        class ConcreteBuilder2 : Builder
+        {
+            private Product _product = new Product();
+
+            public override void BuildPartA()
+            {
+                _product.Add("PartX");
+            }
+
+            public override void BuildPartB()
+            {
+                _product.Add("PartY");
+            }
+
+            public override Product GetResult()
+            {
+                return _product;
+            }
+        }
+
+        /// <summary>
+        /// The 'Product' class
+        /// </summary>
+        class Product
+        {
+            private List<string> _parts = new List<string>();
+
+            public void Add(string part)
+            {
+                _parts.Add(part);
+            }
+
+            public void Show()
+            {
+                Console.WriteLine("\nProduct Parts -------");
+                foreach (string part in _parts)
+                    Console.WriteLine(part);
+            }
+        }
+
     }
 }
