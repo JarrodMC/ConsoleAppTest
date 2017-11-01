c3cd1b82eb6bdc8d94943f0355ca6decc957c551
Jarrod <JarrodMC@outlook.com>
2017-11-01 20:12:01 +0200

    Structs

---
+++ MessingWithConsole/TextFileManipulation/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextFileManipulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            S st = new S();
+            st.X = 5;
+            Console.WriteLine(st.X);
+           
+            Console.ReadKey();
+        }
+
+        struct S
+        {
+            int _x;
+
+            public int X
+            {
+                get { return _x; }
+                set
+                {
+                    if(value < 10)
+                    {
+                        _x = value;
+                    }
+                }
+            }
+        };
+    }
+}
